a4966fe88c1af9c0d9c480a79c59f6f0604da394
Tech-Vexy <eveliaveldrine@outlook.com>
2024-10-01 02:21:05 +0300

    Updates

---
+++ pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"15becdbfa030885e55fe4865973dcdc8","url":"public/index.html"},{"revision":"7decab5e7b30d35eb2c8051b1b7d2da3","url":"src/App.css"},{"revision":"330a9b096c0ada7d8b0297fb2d2dcb18","url":"src/index.css"},{"revision":"51b99b7086585251a57fb75ad96a9b62","url":"src/NumberBaseConverter.css"}]);
